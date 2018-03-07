1b224ecb28fe301d1dd446520ec336c01f784354
L1nc0ln <Lincoln-park@gmx.de>
2018-03-07 18:27:21 +0100

    changed Distribution to creating the distribution on the fly, based on a seed. The old code is now in FileDistribution.py

--- masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/Distribution.py
+++ masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/Distribution.py
@@ -3,35 +3,44 @@
 """
 
 import ctypes
+import random
 
 class Distribution:
     '''
     classdocs
     '''
 
-    def __init__(self, file_name, keep_record):
+    def __init__(self, is_random, seed, min_roll, max_roll, keep_record):
         """
-        @param file_name: the file to read from. NOTE: the file is assumed to be in the distributions folder
+        @param seed: the seed to use to create random data
         @param keep_record: store the last read chunk for retrieval
-        IMPORTANT: close the reader by calling closeReader after you're done reading from the file
         """
-        '''prepend the ../ five times to go up to the toplevel directory'''
-        self.reader = open('distributions/' + file_name, 'r')
+        self.is_random = is_random
+        '''only for non-random distribution'''
+        self.current_index = int(min_roll)
+        random.seed(seed)
+        self.min_roll = int(min_roll)
+        self.max_roll = int(max_roll)
         self.keep_record = keep_record
         if self.keep_record:
             self.last_chunk = []
 
     
-    def readChunk(self, chunk_size, data_type):
+    def generateChunk(self, chunk_size, data_type):
         """
-        @param chunk_size: the amount of lines to read
-        @param data_type: the data type to which the read information should be converted
-        @return: a list containing the read lines, converted to the type 'data_type'
+        @param chunk_size: the amount of values to generate
+        @param data_type: the data type to which the generated information should be converted
+        @return: a list containing the generated values, converted to the type 'data_type'
         """
         if data_type != ctypes.c_char_p:
             chunk = []
-            for _ in range(chunk_size):
-                chunk.append(int(self.reader.readline()))
+            if self.is_random:
+                for _ in range(chunk_size):
+                    chunk.append(random.randint(self.min_roll, self.max_roll))
+            else:
+                for index in range(chunk_size):
+                    chunk.append(self.current_index + index)
+                self.current_index += chunk_size
             chunk = (data_type * chunk_size)(*chunk)
             if self.keep_record:
                 self.last_chunk = chunk
@@ -39,21 +48,26 @@ class Distribution:
         else:
             strArrayType = ctypes.c_char_p * chunk_size
             strArray = strArrayType()
-            for index in range(chunk_size):
-                strArray[index] = (self.reader.readline()).encode('UTF8')
+            if self.is_random:
+                for index in range(chunk_size):
+                    strArray[index] = (str(random.randint(self.min_roll, self.max_roll))).encode('UTF8')
+            else:
+                for index in range(chunk_size):
+                    strArray[index] = (str(self.current_index + index)).encode('UTF8')
+                self.current_index += chunk_size
             if self.keep_record:
                 self.last_chunk = strArray
             return strArray
     
     def getLastChunk(self):
         """
-        @return: the last read chunk of data
+        @return: the last generated chunk of data
         """
         return self.last_chunk
-    
-    def closeReader(self):
-        """
-        closes the utilised reader. Call this when you are done reading from the file
-        """
-        self.reader.close()
+
+def createHashingTable(seed, number_of_bits):
+    random.seed(seed)
+    hashing_table = [[random.randint(0, pow(2, number_of_bits)) for _ in range(256)] for _ in range(4)]
+    return hashing_table
+
         
---
+++ masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/FileDistribution.py
@@ -0,0 +1,59 @@
+"""
+@author: Michael Kaspera
+"""
+
+import ctypes
+
+class Distribution:
+    '''
+    classdocs
+    '''
+
+    def __init__(self, file_name, keep_record):
+        """
+        @param file_name: the file to read from. NOTE: the file is assumed to be in the distributions folder
+        @param keep_record: store the last read chunk for retrieval
+        IMPORTANT: close the reader by calling closeReader after you're done reading from the file
+        """
+        '''prepend the ../ five times to go up to the toplevel directory'''
+        self.reader = open('distributions/' + file_name, 'r')
+        self.keep_record = keep_record
+        if self.keep_record:
+            self.last_chunk = []
+
+    
+    def generateChunk(self, chunk_size, data_type):
+        """
+        @param chunk_size: the amount of lines to read
+        @param data_type: the data type to which the read information should be converted
+        @return: a list containing the read lines, converted to the type 'data_type'
+        """
+        if data_type != ctypes.c_char_p:
+            chunk = []
+            for _ in range(chunk_size):
+                chunk.append(int(self.reader.readline()))
+            chunk = (data_type * chunk_size)(*chunk)
+            if self.keep_record:
+                self.last_chunk = chunk
+            return chunk
+        else:
+            strArrayType = ctypes.c_char_p * chunk_size
+            strArray = strArrayType()
+            for index in range(chunk_size):
+                strArray[index] = (self.reader.readline()).encode('UTF8')
+            if self.keep_record:
+                self.last_chunk = strArray
+            return strArray
+    
+    def getLastChunk(self):
+        """
+        @return: the last read chunk of data
+        """
+        return self.last_chunk
+    
+    def closeReader(self):
+        """
+        closes the utilised reader. Call this when you are done reading from the file
+        """
+        self.reader.close()
+        
