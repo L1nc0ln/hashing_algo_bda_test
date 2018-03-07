ac494cc0da982f02d885331c91dda799d679ecaa
L1nc0ln <Lincoln-park@gmx.de>
2018-03-07 17:24:20 +0100

    refactored configreader to go through a list instead of doing each entry manually

--- masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/ConfigReader.py
+++ masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/ConfigReader.py
@@ -23,6 +23,10 @@ class ConfigReader:
         """
         returns all test cases in the config as a list of dicts
         """
+        test_case_options = ['input_type', 'return_type', 'distribution_file', 'distribution_details',
+                             'test', 'num_buckets', 'rho', 'hash_size', 'row_size', 'test_distribution',
+                             'num_tests', 'capacity']
+        test_case_list_options = ['hash_algorithm', 'argument_types']
         test_list = []
         test_number = 1
         while True:
@@ -31,32 +35,21 @@ class ConfigReader:
             except KeyError:
                 break
             test_case = {}
-            test_case['hash_algorithm']         = splitArgumentTypes(currentSection["hash_algorithm"])
-            test_case['input_type']             = currentSection['input_type']
-            test_case['return_type']            = currentSection['return_type']
-            test_case['argument_types']         = splitArgumentTypes(currentSection['argument_types'])
-            test_case['distribution_file']      = currentSection["distribution_file"]
-            test_case['distribution_details']   = currentSection["distribution_details"]
-            test_case['test']                   = currentSection["test"]
+            for test_case_option in test_case_options:
+                if test_case_option in currentSection:
+                    test_case[test_case_option] = currentSection[test_case_option]
+            for test_case_list_option in test_case_list_options:
+                test_case[test_case_list_option] = splitArgumentTypes(currentSection[test_case_list_option])
             if 'seeds' in currentSection:
                 test_case['seeds'] = splitArgumentTypes(self.config['Test ' + str(test_number)]['seeds'])
                 '''make seeds have the same length as hash algorithms, even if the last algorithms have no seed'''
                 if len(test_case['hash_algorithm']) > len(test_case['seeds']):
                     for _ in range(len(test_case['hash_algorithm']) - len(test_case['seeds'])):
                         test_case['seeds'].append('')
+                if 'table_seed' in currentSection:
+                    test_case['table_seed']     = True
             else:
                 test_case['seeds']              = None
-            if test_case['test'] == 'collisions':
-                test_case['num_buckets']        = currentSection['num_buckets']
-            if test_case['test'] == 'hyperloglog':
-                test_case['rho']                = currentSection['rho']
-                test_case['hash_size']          = currentSection['hash_size']
-            if test_case['test'] == 'countMin':
-                test_case['row_size']           = currentSection['row_size']
-            if test_case['test'] == 'bloomFilter':
-                test_case['test_distribution']  = currentSection['test_distribution']
-                test_case['num_tests']          = currentSection['num_tests']
-                test_case['capacity']           = currentSection['capacity']
             test_list.append(test_case)
             test_number += 1
         return test_list
@@ -65,12 +58,10 @@ class ConfigReader:
         """
         returns all the items in the Options section of the config as dict
         """
+        options_list = ['write_output', 'output_file', 'append', 'num_operations', 'chunk_size']
         options_dict = {}
-        options_dict['write_output']    = self.config['Options']["write_output"]
-        options_dict['output_file']     = self.config['Options']["output_file"]
-        options_dict['append']          = self.config['Options']["append"]
-        options_dict['num_operations']  = self.config['Options']["num_operations"]
-        options_dict['chunk_size']      = self.config['Options']['chunk_size']
+        for option in options_list:
+            options_dict[option] = self.config['Options'][option]
         return options_dict
     
 def splitArgumentTypes(to_split):
