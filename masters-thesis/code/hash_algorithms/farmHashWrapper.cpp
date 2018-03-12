1be823527550a756201ed4439d3a57bed3a355d7
L1nc0ln <Lincoln-park@gmx.de>
2018-03-12 16:42:11 +0100

    wrote initial version of wrapper class for farmhash

--- masters-thesis/code/hash_algorithms/farmHashWrapper.cpp
+++ masters-thesis/code/hash_algorithms/farmHashWrapper.cpp
@@ -1,6 +1,19 @@
-#include farmhash.h
+#include "farmhash.h"
+
 extern "C"
 {
+    void hashNumbers(int array_length, unsigned char** array, int* char_len, unsigned int* return_array){
+        int counter = 0;
+        for(counter; counter < array_length; counter++){
+            return_array[counter] = hash32(array[counter], char_len[counter]);
+        }
+    }
 
-
+    void hashNumbersWSeed(int array_length, unsigned int seed, unsigned char** array, int* char_len,
+            unsigned int* return_array){
+        int counter = 0;
+        for(counter; counter < array_length, counter++){
+            return_array[counter] = hash32WithSeed(array[counter], char_len[counter], seed);
+        }
+    }
 }
