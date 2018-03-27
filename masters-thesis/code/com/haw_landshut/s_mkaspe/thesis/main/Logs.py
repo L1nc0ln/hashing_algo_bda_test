dd52f8e09c934cd79beb5cf5c26c21633d668672
L1nc0ln <Lincoln-park@gmx.de>
2018-03-27 13:37:27 +0200

    changing the delimiter in logging is now done by changing only one variable

--- masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/Logs.py
+++ masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/Logs.py
@@ -9,13 +9,15 @@ __result_list__ = ['test_type', 'hash_algorithms', 'num_operations', 'is_random'
                'right_rim', 'right_value', 'streak_1', 'streak_2', 'streak_3', 'streak_4', 'streak_5', 'streak_6', 'streak_7',
                'streak_8', 'streak_9', 'streak_10+', 'max_streak', 'est_dist_elems', 'dist_elems', 'avg_real_count', 'avg_est_count', 'avg_error',
                'max_error', 'bloom_capacity', 'num_bloom_tests', 'false_pos', 'fill_factor', 'count_bit_flip_dist', 'bit_flip_dist']
-__csv_header__ = ['test type, hash algorithm 1, seed 1, hash algorithm 2, seed 2, hash algorithm 3, seed 3, hash algorithm 4,',
-             ' seed 4, hash algorithm 5, seed 5, hash algorithm 6, seed 6, hash algorithm 7, seed 7, num operations, is random,',
-             ' seed, number of elements, minimum roll, maximum roll, time taken hashing, collisions, chi square,',
-             ' chi sq left rim, left rim val, chi sq right rim, right rim val, streak 1, streak 2, streak 3, streak 4, streak 5, streak 6,',
-             ' streak 7, streak 8, streak 9, streak 10+, max streak, estimated distinct elements, distinct elements,',
-             ' average real count, average est count, average error, max error, bloom filter capacity, number of bloom filter tests,',
-             ' false positives, fill factor']
+__csv_header__ = ['test type', 'hash algorithm 1', 'seed 1', 'hash algorithm 2', 'seed 2', 'hash algorithm 3', 'seed 3', 
+                  'hash algorithm 4', 'seed 4', 'hash algorithm 5', 'seed 5', 'hash algorithm 6', 'seed 6', 'hash algorithm 7',
+                  'seed 7', 'num operations', 'is random', 'seed', 'number of elements', 'minimum roll', 'maximum roll',
+                  'time taken hashing', 'collisions', 'chi square', 'chi sq left rim', 'left rim val', 'chi sq right rim',
+                  'right rim val', 'streak 1', 'streak 2', 'streak 3', 'streak 4', 'streak 5', 'streak 6', 'streak 7', 'streak 8',
+                  'streak 9', 'streak 10+', 'max streak', 'estimated distinct elements', 'distinct elements', 'average real count',
+                  'average est count', 'average error', 'max error', 'bloom filter capacity', 'number of bloom filter tests',
+                  'false positives', 'fill factor']
+__delimiter__ = ';'
 
     
 def writeResultCSV(file_name, append, test_results):
@@ -54,12 +56,12 @@ def __create_result_string__(test_results):
                 if key == 'count_bit_flip_dist':
                     if len(test_results[key]) == 32:
                         '''append 32 commata to bring it inline with the header'''
-                        result_string += __listToCsv__(test_results[key]) + ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,'
+                        result_string += __listToCsv__(test_results[key]) + __delimiterTimesX__(31)
                     else:
                         result_string += __listToCsv__(test_results[key])
                 elif key == 'bit_flip_dist':
                     if len(test_results[key]) == 32:
-                        result_string += ',,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,' + __listToCsv__(test_results[key])
+                        result_string += __delimiterTimesX__(31) + __listToCsv__(test_results[key])
                     else:
                         result_string += __listToCsv__(test_results[key])
                 else:   
@@ -67,14 +69,14 @@ def __create_result_string__(test_results):
             elif key == 'hash_algorithms':
                 result_string += __resolveHashAlgorithmColumns__(test_results[key], test_results['seeds']) if 'seeds' in \
                                         test_results else __resolveHashAlgorithmColumns__(test_results[key], None)  
-        result_string += ','
+        result_string += __delimiter__
     result_string += '\n'
     return result_string
 
 def __listToCsv__(items):
     result_string = ''
     for item in items:
-        result_string += str(item) + ','
+        result_string += str(item) + __delimiter__
     return result_string
         
 def __resolveHashAlgorithmColumns__(hash_algorithms, seeds):
@@ -91,13 +93,19 @@ def __resolveHashAlgorithmColumns__(hash_algorithms, seeds):
     for index in range(len(hash_algorithms_list)):
         hash_algo = hash_algorithms_list[index][hash_algorithms_list[index].rfind('/'):]
         try:
-            return_string += hash_algo + ',' + seeds[index] + ','
+            return_string += hash_algo + __delimiter__ + seeds[index] + __delimiter__
         except IndexError:
-            return_string += hash_algo + ',,'
+            return_string += hash_algo + __delimiter__ + __delimiter__
     for _ in range(6 - len(hash_algorithms)):
-        return_string += ', ,'
+        return_string += __delimiter__ + ' ' + __delimiter__
     '''add final comma to seperate from next field'''
-    return_string += ','
+    return_string += __delimiter__
+    return return_string
+
+def __delimiterTimesX__(x):
+    return_string = ''
+    for _ in range(x):
+        return_string += __delimiter__
     return return_string
 
 def __createFuckingCSVHeader__():
@@ -109,11 +117,11 @@ def __createFuckingCSVHeader__():
     complete_header = ''
     for part_string in __csv_header__:
         complete_header += part_string
-    complete_header += ','
+    complete_header += __delimiter__
     for index in range(64):
-        complete_header += str(index) + ' bit flipped, '
+        complete_header += str(index) + ' bit flipped' + __delimiter__
     for index in reversed(range(64)):
-        complete_header += 'times bit ' + str(index) + ' was flipped, '
+        complete_header += 'times bit ' + str(index) + ' was flipped' + __delimiter__
     complete_header += '\n'
     return complete_header
             
