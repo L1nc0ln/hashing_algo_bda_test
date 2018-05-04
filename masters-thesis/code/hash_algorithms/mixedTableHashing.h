259a72ab8cebcc10dcf7110565c3d02ec0a3c1c9
L1nc0ln <Lincoln-park@gmx.de>
2018-05-04 13:54:40 +0200

    added mixedTableHashing algorithm

---
+++ masters-thesis/code/hash_algorithms/mixedTableHashing.h
@@ -0,0 +1,7 @@
+#ifndef MIXED_TABLE_HASHING_HEADER
+#define MIXED_TABLE_HASHING_HEADER
+
+uint32_t mixedTableHashingHash(uint32_t x, uint64_t hash_table_1[4][256],
+                                uint32_t hash_table_2[4][256]);
+
+#endif
