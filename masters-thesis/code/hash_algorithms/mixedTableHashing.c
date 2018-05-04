274f82d743ab08b2f6fe708ce6a7369013fb9953
L1nc0ln <Lincoln-park@gmx.de>
2018-05-04 13:57:22 +0200

    switched the ++ to the back of index in the loop, seems to be no difference and is the usual way

--- masters-thesis/code/hash_algorithms/mixedTableHashing.c
+++ masters-thesis/code/hash_algorithms/mixedTableHashing.c
@@ -4,10 +4,10 @@
 uint32_t mixedTableHashingHash(uint32_t x, uint64_t hash_table_1[4][256],
                                 uint32_t hash_table_2[4][256]){
     uint64_t hash = 0;
-    for(int index = 0; index < 4; ++index, x >>= 8)
+    for(int index = 0; index < 4; index++, x >>= 8)
         hash ^= hash_table_1[index][(uint8_t)x];
     uint32_t derived = hash >> 32;
-    for(int index = 0; index < 4; ++index, derived >>= 8)
+    for(int index = 0; index < 4; index++, derived >>= 8)
         hash ^= hash_table_2[index][(uint8_t)derived];
     return (uint32_t)hash;
 }
