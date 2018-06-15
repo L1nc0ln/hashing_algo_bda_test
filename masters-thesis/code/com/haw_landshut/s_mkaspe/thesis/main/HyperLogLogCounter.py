a7dddb951657ca1d3e2bf384e5a1ac5f6fca25a3
L1nc0ln <Lincoln-park@gmx.de>
2018-06-15 16:31:16 +0200

    fixed some mistakes in the hll algorithm

--- masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/HyperLogLogCounter.py
+++ masters-thesis/code/com/haw_landshut/s_mkaspe/thesis/main/HyperLogLogCounter.py
@@ -30,9 +30,8 @@ class HyperLogLogCounter():
         register_num = value >> self.register_shifts
         leftover_val = int(value & self.mask)
         '''to +1 here or not to +1'''
-        rho = self.register_shifts - leftover_val.bit_length()
-        if self.registers[register_num] < rho:
-            self.registers[register_num] = rho
+        rho = 1 + self.register_shifts - leftover_val.bit_length()
+        self.registers[register_num] = max(self.registers[register_num], rho)
     
     def putHashedValues(self, values):
         """
@@ -54,23 +53,25 @@ class HyperLogLogCounter():
         """
         harm_mean = 0
         for rho in self.registers:
-            harm_mean += pow(2, -self.registers[rho])
+            harm_mean += pow(2, -rho)
         harm_mean = 1.0 / harm_mean
         correction_const = self.__correction_constants__[self.number_registers] if self.number_registers \
                             in self.__correction_constants__ else __getCorrectionFunc__(self.number_registers)
         estimate = correction_const * self.number_registers * self.number_registers * harm_mean
         '''correcting for too large/small values'''
         if estimate < 2.5 * self.number_registers:
+            print("low estimate")
             num_empty_bins = self.registers.count(0)
             estimate = self.number_registers * np.log(self.number_registers/num_empty_bins)
         elif estimate > 1/30 * pow(2, self.__hash_size__):
+            print("high estimate")
             estimate = -pow(2, self.__hash_size__) * np.log(1 - estimate/pow(2, self.__hash_size__))
         return estimate
 
         
 def __getCorrectionFunc__(number):
     """
-    @param number: value for rho for which the correction number should be determined
+    @param number: value for p for which the correction number should be determined
     """
     return 0.7213 / (1.0 + 1.079 / number)
         
